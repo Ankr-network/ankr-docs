5401a058b8feec2fc3af6d9f5194549df1a7e329
Andrey Zhvakin <barmaglot92@gmail.com>
2022-04-06 14:15:12 +0300

    feat(global-menu): MRPC-552

---
+++ src/components/GlobalMenuWrapper/GlobalMenuWrapper.js
@@ -0,0 +1,53 @@
+import { GlobalMenu } from '@ankr.com/global-menu'
+import React, { useLayoutEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+import styles from './GlobalMenuWrapper.module.css'
+
+const getIsMobile = () => window.innerWidth <= 1235
+
+export default function () {
+  const [isVisible, setVisible] = useState(false)
+  const portalContainer = document.createElement('div')
+  portalContainer.classList.add(styles.root)
+  const [isMobile, setMobile] = useState(getIsMobile())
+
+  const renderMenu = ({ before }) => {
+    const portalRoot = document.querySelector('.navbar__brand')
+    if (portalRoot) {
+      if (before) {
+        portalRoot.parentNode.insertBefore(portalContainer, portalRoot)
+      } else {
+        portalRoot.parentNode.insertBefore(
+          portalContainer,
+          portalRoot.nextSibling,
+        )
+      }
+    }
+  }
+
+  const handleResize = () => {
+    renderMenu({ before: !getIsMobile() })
+    setMobile(getIsMobile())
+  }
+
+  useLayoutEffect(() => {
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    setTimeout(() => setVisible(true), 10)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [isVisible, isMobile])
+
+  // useLayoutEffect(() => {
+  //   renderMenu({ before: true })
+  // }, [portalRoot])
+
+  return createPortal(
+    <GlobalMenu isMobile={isMobile} locale="en-US" />,
+    portalContainer,
+  )
+}
