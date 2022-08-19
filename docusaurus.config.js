c03316b28c093f84bb65536176b2e714fc21ff64
gene <galleon.eight@gmail.com>
2022-08-19 14:12:03 +0400

    Products in top bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,7 +119,7 @@
                 label: "Overview",
               },
               {
-                to: "build-blockchain/overview",
+                to: "build-blockchain/products/v2/ui-interaction",
                 label: "Products",
               },
               {
