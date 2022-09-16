34674bcfac8b2c930ce51ed0420c614d93539c17
dllozhkin <denutol@gmail.com>
2022-09-16 18:31:44 +0300

    feat: DOCS-325 Redirect (#648)
    * test build
    
    * feat: DOCS-325 Remove --website-redirect "/docs"
    
    * feat: DOCS-325 Remove --website-redirect "/docs"
    
    * feat: DOCS-325 Update redirections script
    
    * feat: DOCS-325 Update redirections script
    
    * feat: DOCS-325 Fix path
    
    * feat: DOCS-325 Update metadata key
    
    * feat: DOCS-325 Update log
    
    * feat: DOCS-325 Update path
    
    * feat: DOCS-325 Update path
    
    * feat: DOCS-325 Update path
    
    * feat: DOCS-325 Website-Redirect-Location
    
    * feat: DOCS-325 Content-Type
    
    * feat: DOCS-325 Update to
    
    Co-authored-by: Aleksandr Ishchenko <aleksandr@ankr.com>

---
+++ redirects.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3();
+
+const redirects = JSON.parse(fs.readFileSync('./redirects.json'));
+
+redirects.forEach((item) => {
+    console.log('Uploading:', item.from, '->', item.to)
+    s3.putObject({
+        Body: '',
+        Bucket: process.env.DOMAIN,
+        Key: item.from,
+        Metadata: {
+            'Website-Redirect-Location': item.to,
+            'Content-Type': 'text/html',
+        }
+    }, (err, data) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log('Done')
+    })
+})
