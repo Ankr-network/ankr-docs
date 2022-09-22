ea5577988fae8391f55399127a4edf3ba41136ee
Olle Solens <raproid@gmail.com>
2022-09-22 22:37:26 +0300

    DOCS-306: update head in theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -30,46 +30,42 @@ export default {
 // Logo
   logo: logo,
 // Head content and settings
-head: ({ meta, title }) => {
-    const description =
-      "Ankr is a Web3 Infrastructure Company";
-    const title_ =
-      title && !title.startsWith("Ankr")
-        ? title + "Ankr"
-        : "Ankr Docs";
+  head: ({ title, meta }) => {
+    const { route } = useRouter();
+
+    const ogImage =
+      "Ankr Docs"
 
     return (
       <>
-        {/* General */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="Content-Language" content="en" />
-        <title>{title_}</title>
-        <meta name="favicon" content="/favicon.ico" />
-
-        {/* SEO */}
-        <meta name="description" content={description} />
-        <meta name="og:description" content={description} />
-        <meta name="og:title" content={title_} />
-
-        <meta name="apple-mobile-web-app-title" content="Ankr Docs" />
+        {/* Meta-information */}
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
-
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content="Ankr Docs" />
+        <meta
+          name="og:description"
+          content="Ankr Docs" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-        <meta name="twitter:site:domain" content="docs.multichain.wiki" />
-        <meta name="twitter:url" content="https://docs.multichain.wiki" />
-        <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-icon-180x180.png"
+        <meta name="twitter:image" content={ogImage} />
+        <meta
+          name="og:title"
+          content={
+            title ? title + "Ankr Docs" : "Ankr Docs"
+          }
         />
+        <meta name="og:image" content={ogImage} />
+        <meta name="apple-mobile-web-app-title" content="Ankr" />
+
+        {/* Favicons */}
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
         <link
           rel="icon"
           type="image/png"
-          sizes="192x192"
-          href="/favicon/android-icon-180x180.png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
         />
         <link
           rel="icon"
@@ -78,19 +74,15 @@ head: ({ meta, title }) => {
           href="/favicon/favicon-32x32.png"
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon/favicon-96x96.png"
+          rel="apple-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
         />
         <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
         />
-        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
       </>
     );
   },
