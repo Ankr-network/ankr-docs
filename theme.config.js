749c84441b35ad58783803f3ce5102bd1ddebfc4
Olle Solens <raproid@gmail.com>
2022-09-22 22:28:08 +0300

    DOCS-306: add meta to theme.config.js, change "from": "/ to "from": "

--- theme.config.js
+++ theme.config.js
@@ -30,7 +30,70 @@ export default {
 // Logo
   logo: logo,
 // Head content and settings
+head: ({ meta, title }) => {
+    const description =
+      "Ankr is a Web3 Infrastructure Company";
+    const title_ =
+      title && !title.startsWith("Ankr")
+        ? title + "Ankr"
+        : "Ankr Docs";
 
+    return (
+      <>
+        {/* General */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <title>{title_}</title>
+        <meta name="favicon" content="/favicon.ico" />
+
+        {/* SEO */}
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+        <meta name="og:title" content={title_} />
+
+        <meta name="apple-mobile-web-app-title" content="Ankr Docs" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
+        <meta name="twitter:site:domain" content="docs.multichain.wiki" />
+        <meta name="twitter:url" content="https://docs.multichain.wiki" />
+        <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+      </>
+    );
+  },
 //Footer content and settings
   footer: {
     text: <div className="grid grid-cols-1 md:grid-cols-4 gap-8 md:space-x-16 lg:space-x-28">
