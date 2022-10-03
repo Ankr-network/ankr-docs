29a946a9c91ed14c2c684516ab300b4c8b0528b4
Olle Solens <raproid@gmail.com>
2022-10-03 19:53:14 +0300

    DOCS-362: trying to transfer https://github.com/Ankr-network/frontend/pull/1493/files to ankr-docs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/global.css";
 import "nextra-theme-docs/style.css";
 import Prism from 'prism-react-renderer/prism';
+import { initGoogleGtm } from '../components/gtm';
+
+initGoogleGtm();
 
 (typeof global !== "undefined" ? global : window).Prism = Prism
 require("prismjs/components/prism-kotlin")
@@ -9,9 +12,9 @@ require("prismjs/components/prism-solidity")
 require("prismjs/components/prism-shell-session")
 require("prismjs/components/prism-swift")
 
+
 function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-
   return <>{getLayout(<Component {...pageProps} />)}</>;
 }
 
