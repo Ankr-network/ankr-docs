3b5fcb3b3294002b0e192713c47cba76d2bf74a8
Aleksandr Ishchenko <aleksandr@ankr.com>
2024-01-12 18:40:03 +0530

    feat: WEBSITE-496 Sitemap

--- next.config.mjs
+++ next.config.mjs
@@ -24,5 +24,7 @@ export default withNextra({
       use: ['@svgr/webpack'],
     })
     return config
-  }
+  },
+  siteUrl: process.env.SITE_URL || 'https://www.ankr.com/docs',
+  generateIndexSitemap: false,
 })
