f7a8767784f9a5794db9402c231eb920f0e4b4ac
Roman Bondarenko <bask-5@yandex.ru>
2024-05-09 15:10:36 +0200

    feat: SEO-139 Updated footer

--- src/components/FooterMenu.jsx
+++ src/components/FooterMenu.jsx
@@ -6,6 +6,7 @@ import Medium  from '../../public/socials/medium.svg'
 import Reddit  from '../../public/socials/reddit.svg'
 import Instagram  from '../../public/socials/instagram.svg'
 import Youtube  from '../../public/socials/youtube.svg'
+import LinkedIn  from '../../public/socials/linkedin.svg'
 
 const ListItem = ({ children }) => (
   <li>{children}</li>
@@ -34,7 +35,7 @@ const FooterMenu = () => {
       items: [
         { href: 'https://www.ankr.com/remote-procedure-call/', text: 'Web3 API' },
         { href: 'https://www.ankr.com/staking-crypto/', text: 'Staking' },
-        { href: 'https://www.ankr.com/app-chains/', text: 'AppChains' },
+        { href: 'https://www.ankr.com/rollup-as-a-service-raas/', text: 'Scaling Services' },
         { href: 'https://www.ankr.com/ankr-automate/', text: 'Automate' },
         { href: 'https://www.ankr.com/enterprise/', text: 'Enterprise Solutions' },
         { href: 'https://www.ankr.com/docs/chainscanner/block-explorer/', text: 'Block Explorer' },
@@ -46,6 +47,11 @@ const FooterMenu = () => {
         title: 'RPCs',
         items: [
           { href: 'https://www.ankr.com/remote-procedure-call/polygon-matic/', text: 'Polygon' },
+          { href: 'https://www.ankr.com/rpc/eth/', text: 'Ethereum' },
+          { href: 'https://www.ankr.com/rpc/avalanche/', text: 'Avalanche' },
+          { href: 'https://www.ankr.com/rpc/bsc/', text: 'BSC' },
+          { href: 'https://www.ankr.com/rpc/optimism/', text: 'Optimism' },
+          { href: 'https://www.ankr.com/rpc/arbitrumnova/', text: 'Arbitrum' },
           { href: 'https://www.ankr.com/remote-procedure-call/', text: 'See All' },
         ]
     },
@@ -55,8 +61,7 @@ const FooterMenu = () => {
           { href: 'https://www.ankr.com/staking-crypto/ethereum-eth/', text: 'Ethereum' },
           { href: 'https://www.ankr.com/staking-crypto/polygon-matic/', text: 'Polygon' },
           { href: 'https://www.ankr.com/staking-crypto/avalanche-avax/', text: 'Avalanche' },
-          { href: 'https://www.ankr.com/staking-crypto/binance-bnb/', text: 'Binance Smart Chain' },
-          { href: 'https://www.ankr.com/staking-crypto/polkadot-dot/', text: 'Polkadot' },
+          { href: 'https://www.ankr.com/staking-crypto/binance-bnb/', text: 'BSC' },
           { href: 'https://www.ankr.com/staking-crypto/fantom-ftm/', text: 'Fantom' },
           { href: 'https://www.ankr.com/staking-crypto/', text: 'See All' },
         ]
@@ -69,7 +74,8 @@ const FooterMenu = () => {
           { href: 'https://www.ankr.com/case-studies/polygon/', text: 'Polygon' },
           { href: 'https://www.ankr.com/case-studies/sushi/', text: 'SushiSwap' },
           { href: 'https://www.ankr.com/case-studies/meta-apes/', text: 'Meta Apes' },
-          { href: 'https://www.ankr.com/case-studies/', text: 'See All Case Studies' },
+          { href: 'https://www.ankr.com/case-studies/mantle/', text: 'Mantle Network' },
+          { href: 'https://www.ankr.com/case-studies/', text: 'See all case studies' },
         ]
     },
     {
@@ -79,7 +85,6 @@ const FooterMenu = () => {
           { href: 'https://ankr.hashnode.dev/', text: 'Tutorials' },
           { href: 'https://github.com/Ankr-network', text: 'Github' },
           { href: 'https://www.ankr.com/ankr-whitepaper-2.0.pdf', text: 'Ankr Whitepaper 2.0' },
-          { href: 'https://grants.ankr.com/', text: 'Grants & Referrals' },
           { href: 'https://www.ankr.com/assets/', text: 'Brand Assets' },
         ]
       },
@@ -101,7 +106,8 @@ const FooterMenu = () => {
           { href: 'https://medium.com/ankr-network', text: 'Medium', icon: Medium, className: 'flex' },
           { href: 'https://www.reddit.com/r/Ankrofficial/', text: 'Reddit', icon: Reddit, className: 'flex' },
           { href: 'https://www.instagram.com/ankr/', text: 'Instagram', icon: Instagram, className: 'flex' },
-          { href: 'https://www.youtube.com/c/AnkrOfficial', text: 'Youtube', icon: Youtube, className: 'flex' },
+          { href: 'https://www.youtube.com/c/AnkrOfficial', text: 'YouTube', icon: Youtube, className: 'flex' },
+          { href: 'https://www.linkedin.com/company/ankr/', text: 'LinkedIn', icon: LinkedIn, className: 'flex' },
         ]
       },
   ];
